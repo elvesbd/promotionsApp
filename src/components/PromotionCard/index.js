72a8edf44c257341d59b0d0d23769154842aab6e
elves brito <elvesbd@yahoo.com>
2021-06-08 13:05:37 -0300

    featur-3: created component Modal

--- src/components/PromotionCard/index.js
+++ src/components/PromotionCard/index.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 import styles from './styles.module.scss';
 
 
-const PromotionCard = ({ promotion }) => {
+const PromotionCard = ({ promotion, onClickComments }) => {
   return (
     <div key={promotion.id} className={styles.promotionCard}>
       <img src={promotion.imageUrl} alt={promotion.title}/>
-        <div>
+        <div className={styles.promotionCardInfo}>
           <h1>{promotion.title}</h1>
           <span>R$ {promotion.price}</span>
         
@@ -16,18 +16,22 @@ const PromotionCard = ({ promotion }) => {
               "{promotion.comments.length > 0 ? promotion.comments[0].comment : ''}"
             </div>
         
-            <div 
-            className={styles.promotionCardCommentsCount}>
+            <button 
+              className={styles.promotionCardCommentsCount}
+              onClick={onClickComments}
+            >
               {promotion.comments.length}{' '} 
               {promotion.comments.length > 1 ? 'Comentários' : 'Comentário'}
-            </div>
-            <a 
+            </button>
+
+            <button 
               href={promotion.url} 
               target="_blank" 
               rel="noopener noreferrer"
             >
               IR PARA O SITE
-            </a>
+            </button>
+
             <Link to={`/edit/${promotion.id}`}>Editar</Link>
           </footer>
         </div>
