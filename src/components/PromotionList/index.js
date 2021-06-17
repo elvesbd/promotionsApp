e7eebae71287563f60a2e773761e77ddeb719b9a
elves brito <elvesbd@yahoo.com>
2021-06-16 21:49:03 -0300

    implementedbutton deleted promotion

--- src/components/PromotionList/index.js
+++ src/components/PromotionList/index.js
@@ -2,40 +2,51 @@ import { useState } from "react";
 import { PromotionCard } from "../PromotionCard";
 import { PromotionModal } from '../PromotionModal';
 
+import useApi from '../../utils/useApi';
+
 import styles from './styles.module.scss';
 
 
-export function PromotionList({ loading, err, promotions }) {
+export function PromotionList({ loading, err, promotions, refetch }) {
   const [promotionId, setPromotionId] = useState(null);
+  const [deletedPromotion, setDeletedPromotion] = useApi({
+    method: 'DELETE'
+  });
 
   if (err) {
     return <div>Algo Errado</div>
 
-  } else if (promotions === null) {
+  } if (promotions === null || setDeletedPromotion.loading) {
     return <div>Carregando</div>
 
-  } else if (promotions.length === 0) {
+  } if (promotions.length === 0) {
     return <div>Nenhum resultado encontrado</div>
-
-  } else {
-    return (
-      <div className={styles.promotionList}>
-        {promotions.map((promotion) => (
-          <PromotionCard 
-            promotion={promotion} 
-            onClickComments={() => setPromotionId(promotion.id)}
-          />
-        ))}
-        {loading && (
-          <div>Carregando mais promoções...</div>
-        )}
-        {promotionId && (
-          <PromotionModal 
-            promotionId={promotionId} 
-            onCloseModal={() => setPromotionId(null)}
-          />
-        )}
-      </div>
-    );
   }
+
+
+  return (
+    <div className={styles.promotionList}>
+      {promotions.map((promotion) => (
+        <PromotionCard 
+          promotion={promotion} 
+          onClickComments={() => setPromotionId(promotion.id)}
+          onClickDelete={async () => {
+            await deletedPromotion({
+              url: `/promotions/${promotion.id}`
+            });
+            refetch();
+          }}
+        />
+      ))}
+      {loading && (
+        <div>Carregando mais promoções...</div>
+      )}
+      {promotionId && (
+        <PromotionModal 
+          promotionId={promotionId} 
+          onCloseModal={() => setPromotionId(null)}
+        />
+      )}
+    </div>
+  );
 };
